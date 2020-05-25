aae788cd11bedfa7111b163cc9e8920410e95979
David Katreniak <david.katreniak@student.tuke.sk>
2020-05-25 18:28:49 +0200

    final-complete

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,20 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     };
 
+    const emitMediaEvent = (eventType, listeners) => {
+        listeners.forEach((listener) => {
+            const el = document.querySelector(listener);
+            el.components.sound[eventType]();
+        })
+    };
+
+    const activateSoundsForTouch = () => {
+        const sounds = document.querySelectorAll('a-sound')
+        sounds.forEach((soundEl) => {
+            soundEl.components.sound.playSound();
+            soundEl.components.sound.stopSound();
+        })
+    };
 
     scene.addEventListener('loaded', function (e) {
         setTimeout(() => {
